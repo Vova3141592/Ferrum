5c29185f57750f153b65c03c285780c2dbe75d95
Vova <134244133+Vova3141592@users.noreply.github.com>
2023-05-31 18:06:38 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -30,11 +30,11 @@ let push = {};
 
 function explanation(name, place) {
     if (push[place.id] == true) {
-        hideExplanation(place);
+        hideExplanation(place); // если вставлено, скрываем
     } else if (push[place.id] == false) {
-        showExplanation(place);
+        showExplanation(place); // если скрыто, показываем
     } else {
-        pasteExplanation(name, place);
+        pasteExplanation(name, place); // если не определено, вставляем
     };
 };
 
